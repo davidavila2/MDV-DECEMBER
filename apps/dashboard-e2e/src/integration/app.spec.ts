53b1350803a90a1b44613669f8b8cfb7400f7949
davidavila2 <david.avila@briebug.com>
2021-12-01 14:06:48 -0700

    update project name everywhere else

--- apps/dashboard-e2e/src/integration/app.spec.ts
+++ apps/dashboard-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('dasboard', () => {
+describe('dashboard', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('dasboard', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to dasboard!');
+    getGreeting().contains('Welcome to dashboard!');
   });
 });
